7fd4b88be07e363da43b1b3046f7c61902b8f88e
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-04-10 01:35:21 +0800

    Update include.js

--- upload/assets/framework/component/include.js
+++ upload/assets/framework/component/include.js
@@ -1,7 +1,13 @@
 import { WebComponent } from '../component.js';
+import { loader } from '../index.js';
+
+// Config
+const config = await loader.config('default');
 
 class XInclude extends WebComponent {
     static observed = ['src'];
+
+    controller = '';
 
     get src() {
         return this.getAttribute('src');
@@ -12,11 +18,45 @@ class XInclude extends WebComponent {
     }
 
     async render() {
-        let test = await import('./' + this.src + '.js');
+        // Get the source HTML to load
+        if (!this.src) return;
 
-        console.log(test);
+        let controller = await import(config.config_path + this.src + '.js');
 
-        return test;
+        this.controller = new controller.default;
+
+        let response = this.controller.render();
+
+        response.then(this.compile.bind(this));
+
+
+
+    }
+
+    compile(html) {
+        this.innerHTML = html;
+
+        // Attach Events based on elements that have data-bind and data-on attributes
+        let elements = this.querySelectorAll('[data-bind], [data-on]');
+
+        for (let element of elements) {
+            // Binds the element to an attribute by name.
+            if (element.hasAttribute('data-bind')) {
+                this.controller['$' + element.getAttribute('data-bind')] = element;
+
+                element.removeAttribute('data-bind');
+            }
+
+            if (element.hasAttribute('data-on')) {
+                let part = element.getAttribute('data-on').split(':');
+
+                if (part[1] !== undefined && part[1] in this.controller) {
+                    element.addEventListener(part[0], this.controller[part[1]].bind(this.controller));
+
+                    element.removeAttribute('data-on');
+                }
+            }
+        }
     }
 }
 
