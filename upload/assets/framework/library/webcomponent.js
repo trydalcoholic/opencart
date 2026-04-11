85086a148bd74074f3dd51fecbbb2c2e13967e9d
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-04-11 12:06:07 +0800

    Update webcomponent.js

--- upload/assets/framework/library/webcomponent.js
+++ upload/assets/framework/library/webcomponent.js
@@ -21,36 +21,33 @@ export class WebComponent extends HTMLElement {
     }
 
     async update() {
-        let render = (html) => {
-            this.innerHTML = html;
+        let test = await this.render();
+
+        console.log('update');
+        console.log(test);
+
+        this.innerHTML = test;
 
             // Attach Events based on elements that have data-bind and data-on attributes
-            let elements = this.querySelectorAll('[data-bind], [data-on]');
+        let elements = this.querySelectorAll('[data-bind], [data-on]');
 
-            for (let element of elements) {
-                // Binds the element to an attribute by name.
-                if (element.hasAttribute('data-bind')) {
-                    this['$' + element.getAttribute('data-bind')] = element;
+        for (let element of elements) {
+            // Binds the element to an attribute by name.
+            if (element.hasAttribute('data-bind')) {
+                this['$' + element.getAttribute('data-bind')] = element;
 
-                    element.removeAttribute('data-bind');
-                }
+                element.removeAttribute('data-bind');
+            }
 
-                if (element.hasAttribute('data-on')) {
-                    let part = element.getAttribute('data-on').split(':');
+            if (element.hasAttribute('data-on')) {
+                let part = element.getAttribute('data-on').split(':');
 
-                    if (part[1] !== undefined && part[1] in this) {
-                        element.addEventListener(part[0], this[part[1]].bind(this));
+                if (part[1] !== undefined && part[1] in this) {
+                    element.addEventListener(part[0], this[part[1]].bind(this));
 
-                        element.removeAttribute('data-on');
-                    }
+                    element.removeAttribute('data-on');
                 }
             }
-        };
-
-        let response = this.render();
-
-        if (response instanceof Promise) {
-            response.then(render.bind(this));
         }
     }
 
@@ -67,20 +64,26 @@ export class WebComponent extends HTMLElement {
     }
 
     static get observedAttributes() {
+        console.log(this.observed);
+
         return this.observed;
     }
 
     attributeChangedCallback(name, value_old, value_new) {
-        let event = new CustomEvent('[' + name + ']', {
-            bubbles: true,
-            cancelable: true,
-            detail: {
-                value_old: value_old,
-                value_new: value_new
-            }
-        });
+        console.log(`${name} changed from ${value_old} to ${value_new}`);
 
-        // Dispatch the event
-        this.dispatchEvent(event);
+        if (value_old != value_new) {
+            let event = new CustomEvent('[' + name + ']', {
+                bubbles: true,
+                cancelable: true,
+                detail: {
+                    value_old: value_old,
+                    value_new: value_new
+                }
+            });
+
+            // Dispatch the event
+            this.dispatchEvent(event);
+        }
     }
 }
